ac421ff95b996b5474b17dfdd40580745cbc9ec1
VAC <37587399+vacnex@users.noreply.github.com>
2020-07-14 22:57:59 +0700

    Update 0.3
    - Rewrite message passing with  right click and check empty select
    - Put convert hex code in OnMessage code block into a separate function
    - Put create context menu outside OnMessage code block
    - Now in OnMessage code block just update contextMenus title with URL
    - Add code detect single URL or multiple URL when select hex
    - Fix bug auto open link when select hex with funtion accessUrl
    TODO:
    - Manage multiple link when hex selected
    - Add feature for nhentai

--- background.js
+++ background.js
@@ -6,68 +6,65 @@ const isUrl = (str) => {
         return false;  
     }
 }
-
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    switch(request.type) {
-        case "dom-loaded":
-            var decodetext =[];
-            var strhex = request.data.SelectedText
-            for (let i = 0; i < strhex.length; i+=2) {
-                decodetext.push(String.fromCharCode(parseInt(strhex.replace(/\s/g, "").substring(i,i+2), 16)))
-            }
-            var url = decodetext.join('')
-            if (isUrl(url) == true) {
-                var link = url
-                chrome.contextMenus.remove("hextdecode",function () {  
-                    chrome.contextMenus.create({
-                        title: "Go " + link, 
-                        contexts:["selection"], 
-                        id : "hextdecode",
-                        parentId: "MainMenu"
-                    });
-                })
-                
-                
-                
-            }
-            if (isUrl(url) == false)
-            {
-                chrome.contextMenus.remove("hextdecode")
-                chrome.extension.getBackgroundPage().console.log('not link');
-
-            }
-        break;
-    }
-    return true;
-});
-
-
-// function HexConvert(info,tab) {
-//     var strhex = stext
-//     for (let i = 0; i < strhex.length; i+=2) {
-//         decodetext.push(String.fromCharCode(parseInt(strhex.replace(/\s/g, "").substring(i,i+2), 16)))
-//     }
-
-//     if (validURL(decodetext.join('')) == true) {
-//         var newtext = decodetext.join('')
-//         chrome.extension.getBackgroundPage().console.log(newtext);
-//         chrome.contextMenus.create({
-//             title: "Go " + newtext, 
-//             contexts:["selection"], 
-//             id : "SubMenu",
-//             parentId: "MainMenu",
-//             onclick: HexConvert
-//         });
-//     }
-//     if (validURL(decodetext.join('')) == false)
-//     {
-//         chrome.extension.getBackgroundPage().console.log('Error');
-//     }
-// }
-// HexConvert()
 chrome.contextMenus.create({
     title: "Hex Convert", 
     contexts:["selection"],
     id : "MainMenu"
 });
+chrome.contextMenus.create({
+    title: "pls select hex",
+    contexts:["selection"], 
+    enabled : false,
+    id : "hexdecode",
+    parentId: "MainMenu",
+    onclick: accessUrl
+});
+function accessUrl(info,tab) {
+    var selected = info.selectionText
+    var url = h2t(selected)
+    chrome.windows.create({"url": url, "incognito": true})
+}
+function h2t(selectedhex) {
+    var decodetext =[];
+    for (let i = 0; i < selectedhex.length; i+=2) {
+        decodetext.push(String.fromCharCode(parseInt(selectedhex.replace(/\s/g, "").substring(i,i+2), 16)))
+    }
+    var url = decodetext.join('')
+    return url
+}
+
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    var url = h2t(request.SelectedText)
+    if (isUrl(url) == true) {
+        var link = url
+        var linkgroup = []
+        for(let i of link.split(/[\n\s]/))
+            i && linkgroup.push(i);
+        link = linkgroup;
+        if (link.length > 1)
+        {
+            chrome.extension.getBackgroundPage().console.log(link);
+        }
+        else
+        {
+            chrome.extension.getBackgroundPage().console.log('one url '+link);
+            
+                chrome.contextMenus.update("hexdecode",{
+                    title: "go " + link,
+                    enabled : true,
+                })
+        }
+        
+    }
+    if (isUrl(url) == false)
+    {
+        chrome.contextMenus.update("hexdecode",{
+            title: "pls select hex",
+            enabled : false,
+        })
+        chrome.extension.getBackgroundPage().console.log('not link');
+    }
+});
+
 
--- content.js
+++ content.js
@@ -1,8 +1,11 @@
-window.addEventListener("click", function() {
-    chrome.extension.sendMessage({
-        type: "dom-loaded", 
-        data: {
-            SelectedText: window.getSelection().toString()
-        }
-    });
+document.addEventListener("mousedown", function(event){
+    if (event.button !== 2) {
+        return false;
+    }
+    var selected = window.getSelection().toString();
+        if(event.button == 2 && selected != '') {
+            chrome.extension.sendMessage({
+                'SelectedText' : selected,
+            });
+        } 
 }, true);
