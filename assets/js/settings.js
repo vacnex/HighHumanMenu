ab3b0db1f9d3e03adad8daffef0ac0d0e320a986
VAC <37587399+vacnex@users.noreply.github.com>
2020-07-15 16:25:55 +0700

    Settings now saveable

---
+++ assets/js/settings.js
@@ -0,0 +1,20 @@
+var $IncognitoMode = $('#IncognitoMode');
+var $NameorURL = $('#NameorURL');
+chrome.storage.sync.get('IncognitoMode',function(res) {
+    $IncognitoMode.prop('checked',res.IncognitoMode)
+    chrome.extension.getBackgroundPage().console.log(res.IncognitoMode)
+});
+$IncognitoMode.change(function() {
+    chrome.storage.sync.set({'IncognitoMode': $IncognitoMode.prop('checked')}, function() {
+        chrome.extension.getBackgroundPage().console.log('Settings saved');
+    });
+});
+chrome.storage.sync.get('NameorURL',function(res) {
+    $NameorURL.prop('checked',res.NameorURL)
+    chrome.extension.getBackgroundPage().console.log(res.NameorURL)
+});
+$NameorURL.change(function() {
+    chrome.storage.sync.set({'NameorURL': $NameorURL.prop('checked')}, function() {
+        chrome.extension.getBackgroundPage().console.log('Settings saved');
+    });
+});
