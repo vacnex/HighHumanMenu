029b4839e03844ad0896cc94b95c751becffc1ef
VAC <37587399+vacnex@users.noreply.github.com>
2020-07-19 15:15:46 +0700

    Fix bug on right click can not get selectiontext

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-document.addEventListener("mousedown", function(event){
+document.addEventListener("mouseup", function(event){
     if (event.button !== 2) {
         return false;
     }
-    var selected = window.getSelection().toString();
+    setTimeout(function(){
+        var selected = window.getSelection().toString().trim();
         if(event.button == 2 && selected != '') {
             chrome.extension.sendMessage({
                 'SelectedText' : selected,
             });
         } 
+    }, 50);
 }, true);
