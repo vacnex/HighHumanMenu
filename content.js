79232bbfdd8195c2fc747e2dbfc2a1d4a374b181
VAC <37587399+vacnex@users.noreply.github.com>
2020-09-08 15:01:10 +0700

    Fix update link in contextmenu

--- content.js
+++ content.js
@@ -1,13 +1,11 @@
-document.addEventListener("mouseup", function(event){
+document.addEventListener("mousedown", function(event){
     if (event.button !== 2) {
         return false;
     }
-    setTimeout(function(){
-        var selected = window.getSelection().toString().trim();
-        if(event.button == 2 && selected != '') {
-            chrome.extension.sendMessage({
-                'SelectedText' : selected,
-            });
-        } 
-    }, 50);
+    var selected = window.getSelection().toString().trim();
+    if(event.button == 2 && selected != '') {
+        chrome.extension.sendMessage({
+            'SelectedText' : selected,
+        });
+    }
 }, true);
